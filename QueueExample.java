d7d0d465e3190dc3c41db2418c44a79e8696fde2
yuchun8995 <d1348995@o365.fcu.edu.tw>
2025-11-13 18:46:25 +0800

    Update QueueExample.java

--- QueueExample.java
+++ QueueExample.java
@@ -1,47 +1,40 @@
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+// 使用 JCF 的 LinkedList 來實作自己的 Queue
+// 不直接使用 java.util.Queue
 class MyQueue<T> {
 
+    // 實際儲存元素的容器
+    private LinkedList<T> queue;
+
     public MyQueue() {
+        queue = new LinkedList<>();
     }
 
+    // 尾端加入元素
     public void enqueue(T item) {
+        queue.addLast(item);       // 也可以寫 queue.add(item);
     }
 
+    // 從前端取出元素（並移除）
     public T dequeue() {
-        return queue.first();
+        if (queue.isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return queue.removeFirst();
     }
-    
+
+    // 檢查是否為空
     public boolean isEmpty() {
         return queue.isEmpty();
     }
 
-    // 返回队列中的元素数量
+    // 回傳目前元素數量
     public int size() {
         return queue.size();
     }
 }
 
 public class QueueExample {
-    public static void main(String[] args) {
-        MyQueue<Integer> intQueue = new MyQueue<>();
-        //do some test if needed
-    }
-}
-
-
-//我的Junit測試會測這個Object，這邊以下請不要修改
-//---------------------------------------------------------------------------------
-class Person {
-    private String name;
-    private int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{name='" + name + "', age=" + age + "}";
-    }
-}
-//---------------------------------------------------------------------------------
+    public static void m
